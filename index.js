a013c85c5e2e012696f7b5a931c56ebd26a6ed41
kennthedev <kenn.thedev@gmail.com>
2021-09-18 07:32:10 +0300

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+function calculate(){
+
+ let answer = prompt('please enter anount...');
+ 
+ if(answer!=null){
+    document.getElementById(amount).value=answer;
+ }
+
+   answer= a*2;
+
+   answer=document.getElementById(answer).innerHTML;
+    return answer;
+    
+}
+console.log(answer);
